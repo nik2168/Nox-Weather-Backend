ab836c17d5f9bc5382624195382f0d241e6ca863
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-07-09 17:23:19 +0530

    deployment

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://nox-chat-frontend.vercel.app/",
+    "https://noxweatherfrontend-h2m3oyrb5-nikhils-projects-a3fa49d4.vercel.app/",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
