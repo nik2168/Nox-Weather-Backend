697a5c67bf1ec274a72737ab2a62177e6f7ca659
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-07-09 17:56:12 +0530

    zen v

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://noxweatherfrontend-pqgmdewdg-nikhils-projects-a3fa49d4.vercel.app/",
+    "https://noxweatherfrontend.vercel.app/",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
